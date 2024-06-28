70498af9205093424ac4df2a5167607d0ef7a55e
一纸忘忧 <i@ikxin.com>
2024-06-28 15:53:49 +0800

    feat: auto call openai api translate

--- index.ts
+++ index.ts
@@ -1,1 +1,52 @@
-console.log("Hello via Bun!");
+import OpenAI from "openai";
+import fs from "fs";
+import path from "path";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API,
+  baseURL: process.env.OPENAI_BASE_URL,
+});
+
+const extname = ".md";
+const prompt =
+  "将以下 markdown 格式的文档翻译成中文，请严格按照原文 markdown 格式，并且要求在翻译结果的中文和英文单词之间插入空格隔开";
+
+async function translate(content: string) {
+  try {
+    const response = await openai.chat.completions.create({
+      messages: [
+        { role: "system", content: prompt },
+        { role: "user", content },
+      ],
+      model: process.env.OPENAI_MODEL!,
+      max_tokens: 8 * 1024,
+    });
+    return response.choices[0].message.content || "";
+  } catch (error) {
+    return "";
+  }
+}
+
+async function main(targetPath: string) {
+  const isDir = fs.statSync(targetPath).isDirectory();
+
+  const files = isDir
+    ? fs.readdirSync(targetPath).map((file) => path.join(targetPath, file))
+    : [targetPath];
+
+  for (const filePath of files) {
+    const ext = path.extname(filePath);
+    if (ext === extname) {
+      const startTime = Date.now();
+      const content = fs.readFileSync(filePath, "utf8");
+      const translated = await translate(content);
+      fs.writeFileSync(filePath, translated, "utf8");
+      const countTime = Date.now() - startTime;
+      console.log(`已翻译：${path.basename(filePath)}，耗时：${countTime}ms`);
+    } else {
+      console.log(`未翻译：${path.basename(filePath)}`);
+    }
+  }
+}
+
+await main(process.argv[2] || process.cwd());
